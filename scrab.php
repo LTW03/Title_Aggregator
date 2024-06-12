4309e156bd0ca18a6a5b35832b05060d92681ac3
LTW03 <108678641+LTW03@users.noreply.github.com>
2024-06-12 12:33:39 +0800

    Update scrab.php

--- scrab.php
+++ scrab.php
@@ -1,7 +1,34 @@
 <?php
+require "vendor/autoload.php";
 
-$html = file_get_contents('https://sea.mashable.com/');
+use Goutte\Client;
 
-echo $html
+$client = new Client();
+
+$baseUrl = "https://sea.mashable.com/";
+$pageNum = 1;
+$stopDate = strtotime('2022-12-31');
+
+do {
+    $crawler = $client->request("GET", $baseUrl . "?page=" . $pageNum);
+
+    $crawler->filter(".blogroll")->each(function($node) use ($stopDate) {
+        $datePublished = strtotime($node->filter(".datepublished")->text());
+        if ($datePublished >= $stopDate) {
+            $caption = $node->filter(".caption")->text();
+            $link = $node->filter("a")->attr("href");
+
+            echo "Caption: $caption\n </br>";
+            echo "Date Published: " . date("F j, Y", $datePublished) . "\n</br>"; // format date
+            echo "Link: $link\n\n</br>";
+            echo "</br>";
+        } else {
+            
+            return false;
+        }
+    });
+
+    $pageNum++; 
+} while ($crawler->filter("#showmore")->count() && $pageNum <= 10000); 
 
 ?>
