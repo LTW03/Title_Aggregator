2b9904d88e2e01907e24edbb01fdbe6f3347a991
LTW03 <108678641+LTW03@users.noreply.github.com>
2024-06-12 11:54:29 +0800

    basic scrap done

--- testing.php
+++ testing.php
@@ -4,9 +4,15 @@ require "vendor/autoload.php";
 use Goutte\Client;
 $client = new Client();
 
-$crawler = $client ->request("GET", "https://sea.mashable.com/");
+$crawler = $client->request("GET", "https://sea.mashable.com/");
 
-$crawler ->filter(".broll_info")-> each(function($node){
-    echo
-})
+$crawler->filter(".blogroll")->each(function($node) {
+    $caption = $node->filter(".caption")->text();
+    $datePublished = $node->filter(".datepublished")->text();
+    $link = $node->filter("a")->attr("href");
+
+    echo "Caption: $caption\n";
+    echo "Date Published: $datePublished\n";
+    echo "Link: $link\n\n";
+});
 ?>
