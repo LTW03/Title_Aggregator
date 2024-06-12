b6332b44ba98db625446b56f0b56664790d941ed
LTW03 <108678641+LTW03@users.noreply.github.com>
2024-06-12 11:44:52 +0800

    Update testing.php

--- testing.php
+++ testing.php
@@ -2,27 +2,11 @@
 require "vendor/autoload.php";
 
 use Goutte\Client;
-
 $client = new Client();
-$crawler = $client->request("GET", "https://sea.mashable.com/");
 
-$articles = $crawler->filter(".blogroll ARTICLE")->each(function($node) {
-    $title = $node->filter(".broll_info .caption")->text();
-    $date = $node->filter(".broll_info .datepublished")->text();
-    $link = $node->filter("a")->attr("href");
+$crawler = $client ->request("GET", "https://sea.mashable.com/");
 
-    return [
-        'title' => $title,
-        'date' => $date,
-        'link' => $link
-    ];
-});
-
-// Output the results
-foreach ($articles as $article) {
-    echo "Title: " . $article['title'] . "\n";
-    echo "Date: " . $article['date'] . "\n";
-    echo "Link: " . $article['link'] . "\n";
-    echo "---------------------\n";
-}
+$crawler ->filter(".broll_info")-> each(function($node){
+    echo
+})
 ?>
